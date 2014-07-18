9fbf683c80e0fd70612efe8cfafdd85d853d0761
Brandon Swanson <github@brandonswanson.com>
2014-07-17 18:21:01 -0700

    working recursive check, about to move to an object

--- recursionCheck.cpp
+++ recursionCheck.cpp
@@ -8,6 +8,7 @@
 #include <iostream>
 #include <string>
 #include <cstring>
+#include "SwansonLibs/swansonString.hpp"
 using namespace std;
 
 const string PHRASE = "my secret phrase";
@@ -15,54 +16,91 @@ const string PHRASE = "my secret phrase";
 const int numWords = 3;
 
 /*bool isInPhrase (char* element, char* set){
-   //for all elements
+ //for all elements
 
-   //if there are spaces, then break down is in phrase
+ //if there are spaces, then break down is in phrase
 
-   return (std::strcmp(element,set) || subsetExists(char* element, char* set))
+ return (std::strcmp(element,set) || subsetExists(char* element, char* set))
+ }
+ bool subsetExists(char* elemnent, char* set){
+ //break down set, compare element
+
+ }*/
+//bool containedWithin(string element, string set, int position = 0);
+//bool areEqual (string element, string set);
+list<string> nextSmallestSubSets ( string set );
+bool guessIsPart ( string element , list<string> setList );
+bool guessIsPart ( string element , string set );
+bool guessIsEqual ( string element , string set );
+
+list<string> nextSmallestSubSets ( string set ) {
+   list<string> smallerChunks;
+   if ( !(set.find( ' ' ) == string::npos) ) { // there exists spaces, seperate words
+      swansonString::SeperateWords( set , smallerChunks );
+      return smallerChunks;
+   } else { //one word, seperate letters
+      int position = 0;
+      while ( position < set.size() ) {
+         smallerChunks.push_back( set.substr( position , 1 ) );
+         position++;
+      }
+      return smallerChunks;
+   }
 }
-bool subsetExists(char* elemnent, char* set){
-   //break down set, compare element
-
-}*/
-bool containedWithin(string element, string set, int position = 0);
-bool areEqual (string element, string set);
-
-
-bool areEqual (string element, string set){
-   //for all elements
-
-   //if there are spaces, then break down is in phrase
-
-   return ((set == element) || containedWithin(element,set));
+bool guessIsPart ( string element , string set ) {
+   return guessIsPart( element , nextSmallestSubSets( set ) );
 }
-bool containedWithin(string element, string set, int position){
-   //break down set, compare element
-   if (position == element.size()) return (element == set.substr(position,1));
-   else return( ( element == set.substr(position,1) ) || containedWithin(element, set, ++position));
+bool guessIsPart ( string element , list<string> setList ) {
+   if ( setList.empty() )
+      return false;
+   string MySubSet = setList.back();
+   setList.pop_back();
+   return (guessIsEqual( element , MySubSet )
+         || guessIsPart( element , setList ));
 }
+bool guessIsEqual ( string element , string set ) {
+   if ( set.size() == 1 )
+      return (element == set);
+   else
+      return ((element == set) || guessIsPart( element , set ));
+}
+
+/*
+ bool areEqual (string element, string set){
+ //for all elements
+
+ //if there are spaces, then break down is in phrase
+
+ return ((set == element) || containedWithin(element,set));
+ }
+ bool containedWithin(string element, string set, int position){
+ //break down set, compare element
+ if (position == element.size()) return (element == set.substr(position,1));
+ else return( ( element == set.substr(position,1) ) || containedWithin(element, set, ++position));
+ }
+ */
 
 //very close, but what about breaking down the element?
+int main () {
+   cout << "checking recursion" << endl;
 
-int main(){
- cout << "checking recursion" << endl;
+   //test data
+   string sets[] = { "hello there madame", "goodbye", "farewell", "today" };
+   string elements[] = { "o", "e", "g", "good", "hello", "a", "b", "z", "f",
+         "today", "there madame", "hello there madame" };
+   int setNum = 4;
+   int elementNum = 12;
 
- //test data
- string sets[] = {"hello", "goodbye", "farewell", "today"};
- string elements[] = {"o","e","g", "good", "hello" ,"a" ,"b", "z", "f", "today"};
- int setNum = 4;
- int elementNum = 8;
 
- //test loops
- for (int setIndex = 0; setIndex < setNum; ++setIndex) {
-    cout << endl << "set is:" << sets[setIndex] << "    tests:";
-   for (int elementIndex = 0; elementIndex < elementNum; ++elementIndex) {
-      cout << " "<< elements[elementIndex] <<":" << areEqual(elements[elementIndex],sets[setIndex]);
+   //test loops
+   for ( int setIndex = 0 ; setIndex < setNum ; ++setIndex ) {
+      cout << endl << "set is:" << sets[setIndex] << "    tests:";
+      for ( int elementIndex = 0 ; elementIndex < elementNum ;
+            ++elementIndex ) {
+         cout << " " << elements[elementIndex] << ":"
+               << guessIsEqual( elements[elementIndex] , sets[setIndex] );
+      }
    }
- }
-
 
 }
-
-
 
